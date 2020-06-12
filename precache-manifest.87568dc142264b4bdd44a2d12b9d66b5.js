baa69d075ca8c75dad44f0b8ffdd92e2ccb31f91
NhanMai <mvtnhan@gmail.cpm>
2020-06-12 07:27:46 +0700

    Updates

---
+++ precache-manifest.87568dc142264b4bdd44a2d12b9d66b5.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc74b1e3fb52fb635f58e74cea1e5fe5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff40f1fc80feadb299c6",
+    "url": "./static/js/2.dccd324b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dccd324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6b1675987462cb25b0",
+    "url": "./static/js/main.6c7602de.chunk.js"
+  },
+  {
+    "revision": "edd49469e38382f0d39a",
+    "url": "./static/js/runtime-main.0a9ce987.js"
+  },
+  {
+    "revision": "e78626a1bb6f0065e209c4ee6abe9b78",
+    "url": "./static/media/ask-expert.e78626a1.jpg"
+  },
+  {
+    "revision": "0132a610602aaff664c3d07d82ad449a",
+    "url": "./static/media/bg-header.0132a610.jpg"
+  },
+  {
+    "revision": "d9abb26bafdbd7c5cb93a92e690939ac",
+    "url": "./static/media/chat.d9abb26b.svg"
+  },
+  {
+    "revision": "a0ccfe5a1d63e7b339ec27dc2fb932db",
+    "url": "./static/media/clock.a0ccfe5a.svg"
+  },
+  {
+    "revision": "6d3aa9814e48f91fc0c8bc9cb27eb16e",
+    "url": "./static/media/customer-reviews.6d3aa981.jpg"
+  },
+  {
+    "revision": "1a41cd95241f8963037231d608841fd5",
+    "url": "./static/media/facebook.1a41cd95.svg"
+  },
+  {
+    "revision": "0b436349d2450284812ff6b86d4bb237",
+    "url": "./static/media/handbook-alzheimer-treatment.0b436349.jpg"
+  },
+  {
+    "revision": "ebc601b2999ef285efc91937ebeb3f9c",
+    "url": "./static/media/handbook-egg-laying-or-live-birth.ebc601b2.jpg"
+  },
+  {
+    "revision": "9bb06913bd66b157a14db1fc0a867d90",
+    "url": "./static/media/handbook-healthy-breakfast-time.9bb06913.jpg"
+  },
+  {
+    "revision": "951e94461b9382bf9b43d071ff303ff9",
+    "url": "./static/media/handbook-orange.951e9446.jpg"
+  },
+  {
+    "revision": "517a13b630b9b196a805548f5fcf766b",
+    "url": "./static/media/instagram.517a13b6.svg"
+  },
+  {
+    "revision": "4100fad3060e5fd311f9fa9137f7de64",
+    "url": "./static/media/linkedin.4100fad3.svg"
+  },
+  {
+    "revision": "bdef0594508df08186d7b4ea94dacfbf",
+    "url": "./static/media/livolin.bdef0594.png"
+  },
+  {
+    "revision": "1b0ed0ce9f32afbfad0369610d41c5c9",
+    "url": "./static/media/location.1b0ed0ce.svg"
+  },
+  {
+    "revision": "1a10e070c0555d1ce081fb96c8ffce33",
+    "url": "./static/media/logo.1a10e070.jpg"
+  },
+  {
+    "revision": "455f680f5b092ba34572fa8aec2cc405",
+    "url": "./static/media/mail.455f680f.svg"
+  },
+  {
+    "revision": "7dc03787dd810f23a8757c4d9913d4d9",
+    "url": "./static/media/news-ampoule.7dc03787.jpg"
+  },
+  {
+    "revision": "3d6b2751d64a94cb6f13b4e78b7c7905",
+    "url": "./static/media/news-liver.3d6b2751.jpg"
+  },
+  {
+    "revision": "a7be73bf635f1c9d82cfc29bc12da9b9",
+    "url": "./static/media/news-virut.a7be73bf.jpg"
+  },
+  {
+    "revision": "eb43bb861fa8d54cb0c3a52c9373fa2a",
+    "url": "./static/media/phone.eb43bb86.svg"
+  },
+  {
+    "revision": "ceaf8f1a90aec3abc992c6af8eb43996",
+    "url": "./static/media/play.ceaf8f1a.svg"
+  },
+  {
+    "revision": "e2781720ac1daf095f6dcc88d659f265",
+    "url": "./static/media/search.e2781720.svg"
+  },
+  {
+    "revision": "6c667ca243f444bf5711f2659a3c5cf2",
+    "url": "./static/media/share-from-experts1.6c667ca2.jpg"
+  },
+  {
+    "revision": "9bcbd7d686428aff5ef97b857c8e39f8",
+    "url": "./static/media/share-from-experts2.9bcbd7d6.jpg"
+  },
+  {
+    "revision": "cc80d07aa483cb1ea9ec62843f85cdae",
+    "url": "./static/media/share-from-experts3.cc80d07a.jpg"
+  },
+  {
+    "revision": "1af23438ab73fdefaec3c1eb724d8b33",
+    "url": "./static/media/share-from-experts4.1af23438.jpg"
+  },
+  {
+    "revision": "2be4ac28d1971866f43e074740054c0e",
+    "url": "./static/media/twitter.2be4ac28.svg"
+  }
+]);
